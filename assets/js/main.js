f0620e4cc40d2a05db61a5869c80a2940c31e5dd
haciyev13 <ehedhaciyev001@gmail.com>
2024-11-13 18:08:43 +0400

    bitdi

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,9 @@ let icon = document.querySelector(".filter-icon");
 let main_content = document.querySelector(".main__content-form");
 const menuIcon = document.querySelector('.nav__menu');
 const sidebar = document.querySelector('.sidebar');
-let sectionCards = document.querySelector(".section__card-list")
-
-
-
-let slider = document.querySelector(".section__card-list-slider");
+let sectionCards = document.querySelector(".section__card-list-item")
+let sectionCardCategory = document.querySelector(".category-container")
+let sectionCardCompanies = document.querySelector(".section__card-list-slider")
 
 let main__content_UL = document.querySelectorAll(".main__content-form>form>label>ul")
 
@@ -57,53 +55,47 @@ icon.addEventListener("click", function (e) {
 })
 
 
-$('.section__card-list-slider').slick({
-     slidesToShow: 5,
-     slidesToScroll: 1,
-     responsive: [
-          {
-               breakpoint: 666,
-               settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-               }
-          }
-     ]
-});
-$('.category-container').slick({
-     slidesToShow: 5,
-     slidesToScroll: 1,
-     responsive: [
-          {
-               breakpoint: 666,
-               settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-               }
-          }
-     ]
-});
 
 
 
-// function getFetch(endpoint, func) {
-//      fetch(`http://localhost:3000/${endpoint}`)
-//           .then(res => res.json())
-//           .then(data => {
-//                console.log(func(data));
-//           })
-// }
-
-// getFetch("vakansiya_data");
 
 
-function getIndexData() {
-     let empthyData = '<h2>Vakansiyalar</h2>'
-     fetch("http://localhost:3000/vakansiya_data")
+function getURL() {
+     let url = new URLSearchParams(window.location.search)
+     let pathName = window.location.pathname.split("/").pop();
+
+     return {
+          path: pathName
+     }
+
+}
+
+let params = getURL()
+
+
+
+
+let BASE_URL = "http://localhost:3000/";
+
+
+function getFetch(endpoint, func) {
+     fetch(`${BASE_URL}${endpoint}`)
           .then(res => res.json())
           .then(data => {
-               data.map(cards => {
-                    empthyData += `
+               func(data);
+          })
+}
+
+getFetch("vakansiya_data", getIndexData);
+
+
+function getIndexData(data) {
+     let empthyData = '<h2>Vakansiyalar</h2>'
+     data.map(cards => {
+
+          let cardInfoHtml = cards.card_info.map(info => `<h4>${info}</h4>`).join("");
+
+          empthyData += `
                     <li class="section__card-item">
                                    <div class="section__card-content flex__space-between">
                                         <a href="#" class="section__card-link">
@@ -122,9 +114,7 @@ function getIndexData() {
                                         <div class="section__card-main-location">
                                              <p class="section__card-main-location-title">${cards.card_location}</p>
                                              <div class="flex__center section__card-main-location-text">
-                                                  ${cards.card_info.map(item=>{
-                                                       `<h4>${item}</h4>`
-                                                  })}
+                                             ${cardInfoHtml}
                                              </div>
                                         </div>
                                         <div class="section__card-main-apply">
@@ -135,11 +125,79 @@ function getIndexData() {
                                    </div>
                               </li>
                     `
-               })
+     })
 
-               const sectionCards = document.querySelector(".section__card-list");
-               sectionCards.innerHTML = empthyData;
 
-          })
+     sectionCards.innerHTML = empthyData;
+
 }
-getIndexData()
+
+function getIndexCatData(data) {
+     data.map(item => {
+          sectionCardCategory.innerHTML += `
+           <a href="">
+                    <div class="category-card">
+                    <i class="fas fa-dollar-sign category-icon"></i>
+                    <h3>${item.name}</h3>
+                    <p>${item.count} Vakansiya</p>
+               </div>
+          </a>
+          `
+     })
+
+     $('.category-container').slick({
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          responsive: [
+               {
+                    breakpoint: 666,
+                    settings: {
+                         slidesToShow: 2,
+                         slidesToScroll: 1
+                    }
+               }
+          ]
+     });
+}
+
+function getIndexCompanieData(data) {
+     data.map(item => {
+          sectionCardCompanies.innerHTML += `
+          <li class="section__card-item section__card-item-company">
+                    <a href="#" class="section__card-link slider__card-link">
+                         <div class="company-description">
+                              <span><i class="fa-solid fa-briefcase"></i></span>
+                              <h4>${item.name}</h4>
+                         </div>
+                    </a>
+               <div class="company-footer">
+                    <a href="">${item.count} Vakansiya</a>
+               </div>
+          </li>
+          `
+     })
+
+     $('.section__card-list-slider').slick({
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          responsive: [
+               {
+                    breakpoint: 666,
+                    settings: {
+                         slidesToShow: 2,
+                         slidesToScroll: 1
+                    }
+               }
+          ]
+     });
+}
+
+
+if (params.path == "index.html") {
+     getFetch("vakansiya_data", getIndexData)
+     getFetch("work_category", getIndexCatData)
+     getFetch("companies", getIndexCompanieData)
+}
+
+
+
